864cbde35825c815b0c1703c41ca9f40475a18c0
Eric Anger <eanger@gmail.com>
2015-06-17 14:27:00 -0400

    finished lexer, on to parsing

--- crisp.py
+++ crisp.py
@@ -1,21 +1,70 @@
 #!/usr/bin/env python3
-from collections import namedtuple
-import operator
 
-operators = {'+' : operator.add,
-             '-' : operator.sub,
-             '*' : operator.mul,
-             '/' : operator.floordiv}
-def parse(line):
+from enum import Enum
+
+Lexeme = Enum('Lexeme', 'Operator LParen RParen Number')
+
+def addFunction(*args):
+    result = 0
+    for arg in args:
+        result = result + arg
+    return result
+
+def lex(line):
+    if line == "":
+        return []
+
+    def readNumber(l):
+        """ Return end idx of number read from l """
+        end_idx = 0
+        for char in l:
+            if char in "0123456789":
+                end_idx = end_idx + 1
+        return end_idx
+    
+    # do some lexing, return new list combined with further lexing
+    char = line[0]
+    lexing_idx = 1
+    if char == " ":
+        # white space, continue
+        return lex(line[lexing_idx:])
+
+    if char == "+":
+        res = (Lexeme.Operator, addFunction)
+    if char == "-":
+        res = (Lexeme.Operator, operator.sub)
+    if char == "*":
+        res = (Lexeme.Operator, operator.mul)
+    if char == "/":
+        res = (Lexeme.Operator, operator.truediv)
+    if char in "0123456789":
+        num_idx = readNumber(line)
+        res = (Lexeme.Number, int(line[:num_idx]))
+        lexing_idx = num_idx
+
+    return [res] + lex(line[lexing_idx:])
+
+def parseCommand(tokens):
+    # recursive parsing
+    pass
+
+def evaluate(ast):
+    pass
+
+def eval(line):
     # precondition: line has no beginning or trailing white space
     print("You entered: {}".format(line))
-    command = Expression(operators[line[0]],
+    tokens = lex(line)
+    print(tokens)
+    ast = parse(tokens)
+    result = evaluate(ast)
+    print(result)
 
 if __name__ == "__main__":
     print("Welcome to crisp")
     try:
         while True:
             line = input("crisp> ")
-            parse(line.strip())
+            eval(line.strip())
     except KeyboardInterrupt:
         print("\nShutting down.")
--- notes.md
+++ notes.md
@@ -1,9 +1,27 @@
+# Lexing
+- This happens first, to turn a string into a list of tokens
+    tokens = [(string, category),...]
+- Turns into parser objects
+    Operator: + - * /
+    Lparen: (
+    Rparen: )
+    Number: [-] 0-9... [.] 0-9...
+
 # Parser
 - start with polish notation calculator
     Command: Operator Expression...
-    Expression: Number or '(' Operator Expression... ')'
+    Expression: Number or '(' Command ')'
     Number: [-]0-9...
     Operator: '+' '-' '*' '/'
+- What's the data structure??
+    class SExpression:
+        def eval(self):
+            return self.operand
+
+    eg "* 13 2"
+    (operator.mul, 13, 2)
+    "* (+ 12 3) 4"
+    (operator.mul, (operator.plus, 12, 3), 4)
 
 
 class Atom:
