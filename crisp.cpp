4679157a79ad77a5f040fee38623c682449190fe
Eric Anger <eanger@gmail.com>
2015-06-17 16:59:01 -0400

    implement booleans

--- crisp.cpp
+++ crisp.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <cctype>
 #include <iostream>
+#include <stack>
 #include <string>
 
 using namespace std;
@@ -9,13 +10,22 @@ class Value {
   public:
     enum class Type {
       FIXNUM,
+      BOOLEAN,
     };
     Type type;
     union {
       long fixnum;
+      bool boolean;
     };
+
+    explicit Value(long num) : type{Type::FIXNUM}, fixnum{num} {}
+    explicit Value(bool b) : type{Type::BOOLEAN}, boolean{b} {}
+    Value() {}
   private:
 };
+
+Value True(true);
+Value False(false);
 
 class Expression {
   public:
@@ -27,29 +37,50 @@ bool isDelimiter(char x) {
   return x == ' ';
 }
 
-Expression read(string line) {
-  long sign = 1;
-  unsigned idx = 0;
+Expression read(stack<char> input) {
   Expression expr;
-  expr.val.type = Value::Type::FIXNUM;
-  expr.val.fixnum = 0l;
-  if(line[0] == '-'){
-    sign = -1;
-    ++idx;
-  }
-  for(; idx < line.size(); ++idx){
-    auto character = line[idx];
-    if(isdigit(character)){
-      expr.val.fixnum = expr.val.fixnum * 10 + (character - '0');
+  char character = input.top();
+  input.pop();
+
+  if(character == '#'){ // read a boolean
+    character = input.top();
+    input.pop();
+    if(character == 't'){
+      expr.val = True;
+    } else if(character == 'f'){
+      expr.val = False;
     } else {
-      break;
+      cerr << "Error: Unknown bool literal '" << character << "'\n";
+      exit(-1);
+    }
+  } else {
+    long sign = 1;
+    if(character == '-'){
+      sign = -1;
+      character = input.top();
+      input.pop();
+    }
+    if(isdigit(character)){ // read a number
+      expr.val.type = Value::Type::FIXNUM;
+      expr.val.fixnum = character - '0';
+      while(!input.empty()){
+        character = input.top();
+        input.pop();
+        if(isdigit(character)){
+          expr.val.fixnum = expr.val.fixnum * 10 + (character - '0');
+        } else {
+          input.push(character);
+          break;
+        }
+      }
+      expr.val.fixnum *= sign;
     }
   }
-  if(idx != line.size() && !isDelimiter(line[idx])){
-    cerr << "Error: Invalid character '" << line[idx] << "'\n";
+  if(!input.empty() && !isDelimiter(input.top())){
+    cerr << "Error: Invalid character '" << input.top() << "'\n";
     exit(-1);
   }
-  expr.val.fixnum *= sign;
+
   return expr;
 }
 
@@ -61,6 +92,13 @@ void print(Value value) {
   switch(value.type){
     case Value::Type::FIXNUM:{
       cout << value.fixnum << "\n";
+    } break;
+    case Value::Type::BOOLEAN:{
+      if(value.boolean){
+        cout << "True\n";
+      } else {
+        cout << "False\n";
+      }
     } break;
     default:{
       assert(false && "Invalid Value type");
@@ -75,7 +113,11 @@ int main(int, char*[]) {
     cout << "crisp> ";
     string line;
     getline(cin, line);
-    auto ast = read(line);
+    stack<char> input;
+    for(auto x = line.rbegin(); x != line.rend(); ++x){
+      input.push(*x);
+    }
+    auto ast = read(input);
     auto res = eval(ast);
     print(res);
   }
